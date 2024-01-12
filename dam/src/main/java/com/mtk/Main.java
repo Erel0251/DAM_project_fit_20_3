17a82fe83f1cfa2bacf021a73f660217d5e292f8
mhneh <20120075@student.hcmus.edu.vn>
2024-01-12 17:47:10 +0700

    Add example

--- dam/src/main/java/com/mtk/Main.java
+++ dam/src/main/java/com/mtk/Main.java
@@ -14,39 +14,40 @@ import com.mtk.sample.User;
 import java.lang.reflect.InvocationTargetException;
 import java.sql.SQLException;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class Main {
     public static void main(String[] args) throws OutOfConnectionException, SQLException, InstantiationException, IllegalAccessException, UnsupportedActionException, InvocationTargetException, NoSuchMethodException {
-//        Map<Object, Object> params = new HashMap<>();
-//        params.put("createDatabaseIfNotExist", "true");
-//        ConnectConfiguration configuration = ConnectConfiguration.builder(ConnectionType.MySQL)
-//                .hostName("localhost:3306")
-//                .username("benjamin")
-//                .password("benjamin")
-//                .databaseName("mtk-demo")
-//                .params(params)
-//                .build();
-//        try {
-//            Persistence.configureDatasource(configuration);
-//        } catch (ConnectionException e) {
-//            System.out.println(e.getMessage());
-//            throw new RuntimeException(e);
-//        }
-//        RecordManagerFactory factory = Persistence.createRecordManagerFactory("simple-dao");
-//        RecordManager recordManager = factory.createRecordManager();
-//        QueryBuilder query = QueryBuilder.update("user")
-//                .setter("username", "hehe")
-//                .where("id = 3");
-//        recordManager.executeUpdate(query);
-//        QueryBuilder insertQuery = QueryBuilder.insert("user")
-//                .value("username", "test");
-//        recordManager.executeUpdate(insertQuery);
-//
-//        User user = new User();
-//        recordManager.insert(user);
-//        List<User> users = recordManager.executeQuery(query, User.class);
-//        users.forEach(user -> System.out.println(user.getId() + " " + user.getName()));
+        Map<Object, Object> params = new HashMap<>();
+        params.put("createDatabaseIfNotExist", "true");
+        ConnectConfiguration configuration = ConnectConfiguration.builder(ConnectionType.MySQL)
+                .hostName("localhost:3306")
+                .username("root")
+                .password("auth-service-root-pass-123")
+                .databaseName("mtk-demo")
+                .params(params)
+                .build();
+        try {
+            Persistence.configureDatasource(configuration);
+        } catch (ConnectionException e) {
+            System.out.println(e.getMessage());
+            throw new RuntimeException(e);
+        }
+        RecordManagerFactory factory = Persistence.createRecordManagerFactory("simple-dao");
+        RecordManager recordManager = factory.createRecordManager();
+        QueryBuilder query = QueryBuilder.update("user")
+                .setter("username", "hehe")
+                .where("id = 3");
+        recordManager.executeUpdate(query);
+        QueryBuilder insertQuery = QueryBuilder.insert("user")
+                .value("username", "test");
+        recordManager.executeUpdate(insertQuery);
+
+        User user = new User();
+        recordManager.insert(user);
+        List<User> users = recordManager.executeQuery(QueryBuilder.select().from("user"), User.class);
+        users.forEach(u -> System.out.println(u.getId() + " " + u.getName()));
 
         QueryBuilder selectQuery = QueryBuilder.select()
                 .from("user")
